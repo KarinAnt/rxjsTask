049a31a28f1f97e7e161906a07e5c1c0afaa0f2f
KarinAnt <antonyankarinka@gmail.com>
2019-11-07 19:31:35 +0400

    urll

--- src/app/example.service.ts
+++ src/app/example.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { from, Subject, BehaviorSubject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { switchMap, filter } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
--- src/app/mein/mein.component.ts
+++ src/app/mein/mein.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ExampleService } from '../example.service';
+import { from} from 'rxjs';
+import { switchMap, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-mein',
@@ -12,7 +14,7 @@ export class MeinComponent implements OnInit {
   constructor(private example : ExampleService) {
     // console.log(example.color.getValue());
     example.getConfig().pipe(
-
+      switchMap(data => Object.entries(data).filter(([key,value]) => key=="reg.general.document_proccessing_error"))
     ).subscribe(x=> this.urll = x);
     
    }
